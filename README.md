cb1ed82ab71f1870fa1f760b49c9dec7c0b6356f
Marina Puig de Morales Marinković <mpmarinkovic@gmail.com>
2020-02-17 09:27:57 -0500

    changes on README

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # javascript-challenge
+
+This repository contains two folders UFO-level-1 and UFO-level-2 . Each contains a basic HTML web page or use [index.html] and folders [images] containing the images, [css] format, and [js] containing UFO dataset [data.js]data and the javascript code [app.js].
+
+The app.js provides the code to create a dinamic table. In addition the code allows the user to filter the data based on the variable `date/time`.  The UFO-level-2 contains the same architecture but the user can filter by the following variables:  
+  1. `date/time`
+  2. `city`
+  3. `state`
+  4. `country`
+  5. `shape`
+
+Enjoy the html and the UFO reports!!
+
+
+
--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,7 +1,7 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// construct the tables as a function that can be called everytime and option is selected
 var tbody = d3.select("tbody");
 
 function buidTable(data) {
@@ -16,15 +16,41 @@ function buidTable(data) {
 }
 
 buidTable(tableData);
-
 var button = d3.select("#filter-btn");
 button.on("click", filterData);
 
 function filterData() {
     var dateInput = d3.select("#datetime");
     var dateInputValue = dateInput.property("value");
-    var filteredDate = tableData.filter(ufo => ufo.datetime === dateInputValue);
-    buidTable(filteredDate);
+    var cityInput = d3.select("#city");
+    var cityInputValue = cityInput.property("value");
+    var stateInput = d3.select("#state");
+    var stateInputValue = stateInput.property("value");
+    var countryInput = d3.select("#country");
+    var countryInputValue = countryInput.property("value");
+    var shapeInput = d3.select("#shape");
+    var shapeInputValue = shapeInput.property("value");
+    console.log(shapeInputValue)
+    // if (dateInputValue !== '' ){
+    //     var filtered = tableData.filter(ufo => ufo.datetime === dateInputValue);
+    //     buidTable(filtered);
+    // }
+    // else if (cityInputValue !== '') {
+    //     var filtered = tableData.filter(ufo => ufo.city === cityInputValue);
+    //     buidTable(filtered);
+    // }
+    // else if (stateInputValue !== '') {
+    //     var filtered = tableData.filter(ufo => ufo.state === stateInputValue);
+    //     buidTable(filtered);
+    // }
+    // else if  (countryInputValue !== '') {
+    //     var filtered = tableData.filter(ufo => ufo.country === countryInputValue);
+    //     buidTable(filtered);
+    // }
+    // else (shapeInputValue !== '') { 
+    //     var filtered = tableData.filter(ufo => ufo.shape === shapeInputValue);
+    //     buidTable(filtered);
+    // }
 }
 
 
